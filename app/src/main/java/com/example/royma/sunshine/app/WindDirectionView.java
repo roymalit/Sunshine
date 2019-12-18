6fa25b74c5bcf0845d1b85e8345db7cd9f635829
Roy M <roymalit@hotmail.com>
2019-12-18 19:53:20 +0000

    Compass drawable developed further. To be completed and implemented after release.

--- app/src/main/java/com/example/royma/sunshine/app/WindDirectionView.java
+++ app/src/main/java/com/example/royma/sunshine/app/WindDirectionView.java
@@ -4,22 +4,29 @@ import android.content.Context;
 import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.Shader;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 
+import java.util.Arrays;
+import java.util.List;
+
 class WindDirectionView extends View {
+    private final String LOG_TAG = WindDirectionView.class.getSimpleName();
+
     private Paint textPaint;
-    private Paint compassPaint;
+    private Paint compassPaintTop;
+    private Paint compassPaintBottom;
     private Paint compassBorder;
     private float textHeight;
     private Paint shadowPaint;
-    private Path mCirle;
+    private Path mCircle;
+    private RectF mRectF;
+    private List<String> directionText;
 
     public WindDirectionView(Context context) {
         this (context, null);
@@ -28,28 +35,38 @@ class WindDirectionView extends View {
     public WindDirectionView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
 
-        mCirle = new Path();
+        mCircle = new Path();
+        mRectF = new RectF();
+
+        // Compass directions
+        String[] directions = {"N", "NNE", "NE", "ENE","E","ESE","SE","SSE",
+                "S","SSW","SW","WSW","W","WNW","NW","NNW"};
+        directionText = Arrays.asList(directions);
 
         // Set up paints for canvas drawing
         textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        textPaint.setColor(Color.BLACK);
+        textPaint.setColor(Color.WHITE);
         textPaint.setStyle(Paint.Style.FILL_AND_STROKE);
         if (textHeight == 0) {
-            textHeight = textPaint.getTextSize();
+            textPaint.setTextSize(35f);
         } else {
-            textPaint.setTextSize(textHeight);
+            textHeight = textPaint.getTextSize();
         }
-        //textPaint.setTextSize(40f);
 
-        int[] colors = {
-                Color.RED,
-                Color.BLUE
-        };
-        compassPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        compassPaint.setShader(new LinearGradient(0, 0, 500, 0, colors, null, Shader.TileMode.CLAMP));
-        compassPaint.setTextSize(textHeight);
+        // Top half of compass
+        compassPaintTop = new Paint(Paint.ANTI_ALIAS_FLAG);
+        compassPaintTop.setColor(Color.rgb(32, 89, 232));
+        compassPaintTop.setTextSize(textHeight);
 
+        // Bottom half of compass
+        compassPaintBottom = new Paint(Paint.ANTI_ALIAS_FLAG);
+        compassPaintBottom.setColor(Color.rgb(232, 112, 32));
+        compassPaintBottom.setTextSize(textHeight);
+
+        // Compass border
         compassBorder = new Paint(Paint.ANTI_ALIAS_FLAG);
+        compassBorder.setStyle(Paint.Style.STROKE);
+        compassBorder.setStrokeWidth(textPaint.getTextSize() + 10);
         compassBorder.setColor(Color.DKGRAY);
         compassBorder.setTextSize(textHeight);
 
@@ -81,23 +98,39 @@ class WindDirectionView extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        //TODO: Draw rest of compass - 2 half-circles, labels in-between, gradient ring
-        int x = getWidth() / 2;
-        int y = getHeight() / 2;
-        int radius = y;
-        mCirle.addCircle(x,y, radius, Path.Direction.CW);
+        //TODO: Draw rest of compass - direction labels, centre lines (implement after app release)
+        float x = getWidth() / 2;
+        float y = getHeight() / 2;
+        float radius = y;
+        mCircle.addCircle(x,y, radius, Path.Direction.CW);
+        float textPadding = textPaint.getTextSize();
+        double circlePerimeter = Math.PI * radius / 8;  // 2*PI*r / 16 pieces
 
         // Rotate canvas so drawing starts from top
         canvas.rotate(-90, x, y);
 
-        //canvas.drawCircle(x, y, radius, compassBorder);
+        canvas.drawCircle(x, y, radius-compassBorder.getStrokeWidth(), compassPaintTop);
 
-        canvas.drawCircle(x, y, radius-35, compassPaint);
+        // Rectangle to draw semi-circle within
+        mRectF.set(x-radius, y-radius, x+radius,y+radius);
+        // Draw bottom semi-circle
+        canvas.drawArc(mRectF, 90, 180, true, compassPaintBottom);
 
+        // Draw compass border. Halved compass border = half of text height taken into account
+        canvas.drawCircle(x, y, radius-compassBorder.getStrokeWidth()/2, compassBorder);
+
+        double currentAngle = 0.0;
         // Draw the label text
-        canvas.drawTextOnPath("Draw text along path " +
-                "starting from the origin and ending wherever", mCirle, 0, textPaint.getTextSize(), textPaint);
-        //canvas.drawPath(mCirle, textPaint);
+        for (int i = 0; i < directionText.size(); i++){
+
+            canvas.drawTextOnPath(directionText.get(i), mCircle, (float) currentAngle, textPadding, textPaint);
+
+            Log.d(LOG_TAG, "currentAngle(" + i + "): " + currentAngle);
+            currentAngle+=circlePerimeter;
+        }
+
+        // Draw compass lines
+        // canvas.drawPath();
     }
 
     /**
